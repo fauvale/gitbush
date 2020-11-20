6a3092b02d3b6db29df9ffb29da180b70a6e2c97
Bichaeva Valeria Alexeevna <valerabichaeva@gmail.com>
2020-11-21 05:05:54 +0700

    Функция 3

--- main.cpp
+++ main.cpp
@@ -2,40 +2,55 @@
 
 using namespace std;
 
-double my_pow(double x, unsigned int n);
+double my_pow(double x, int n);
 
 int main()
 {
-        double x, res;
-        int n;
+    double x, res;
+    int n;
 
-        cout << "Введите число: ";
-        cin >> x;
-        cout << "Введите степень: ";
-        cin >> n;
+    cout << "Введите число: ";
+    cin >> x;
+    cout << "Введите степень: ";
+    cin >> n;
 
-        res = my_pow(x, n);
-        cout << "Результат: " << res << endl;
+    res = my_pow(x, n);
+    cout << "Результат: " << res << endl;
 
-        return 0;
+    return 0;
 }
 
-double my_pow(double x, unsigned int n)
+double my_pow(double x, int n)
 {
-        double count = 1;
-        if (!n) return 1;
-        while (n)
-        {
-                if (n % 2 == 0)
-                {
-                        n /= 2;
-                        x *= x;
-                }
-                else
-                {
-                        n--;
-                        count *= x;
-                }
+    double result = 1;
+    if (n < 0) {
+        n = -n;
+
+        while (n) {
+            if (n % 2 == 0) {
+                n /= 2;
+                x *= x;
+            }
+            else {
+                n--;
+                result *= x;
+            }
         }
-        return count;
+
+        return 1 / result;
+    }
+    else {
+        while (n) {
+            if (n % 2 == 0) {
+                n /= 2;
+                x *= x;
+            }
+            else {
+                n--;
+                result *= x;
+            }
+        }
+
+        return result;
+    }
 }

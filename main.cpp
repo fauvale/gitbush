1bdc2fcf6c673eaff4afcc9705d7728b6201015c
Bichaeva Valeria Alexeevna <valerabichaeva@gmail.com>
2020-11-21 04:59:04 +0700

    Функция 2

--- main.cpp
+++ main.cpp
@@ -6,28 +6,36 @@ double my_pow(double x, unsigned int n);
 
 int main()
 {
-	double x, res;
-	int n;
+        double x, res;
+        int n;
 
-	cout << "Введите число: ";
-	cin >> x;
-	cout << "Введите степень: ";
-	cin >> n;
+        cout << "Введите число: ";
+        cin >> x;
+        cout << "Введите степень: ";
+        cin >> n;
 
-	res = my_pow(x, n);
-	cout << "Результат: " << res << endl;
+        res = my_pow(x, n);
+        cout << "Результат: " << res << endl;
 
-	return 0;
+        return 0;
 }
 
 double my_pow(double x, unsigned int n)
 {
-	if (n == 0)
-		return 1;
-	else if (n == 1)
-		return x;
-	else if (n % 2 == 0)
-		return pow(x * x, n / 2);
-	else
-		return pow(x * x, n / 2) * x;
+        double count = 1;
+        if (!n) return 1;
+        while (n)
+        {
+                if (n % 2 == 0)
+                {
+                        n /= 2;
+                        x *= x;
+                }
+                else
+                {
+                        n--;
+                        count *= x;
+                }
+        }
+        return count;
 }
